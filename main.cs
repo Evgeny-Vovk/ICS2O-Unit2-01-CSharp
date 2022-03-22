09bb83269b4df341cf9783a25f6663d5cf826459
Evgeny-Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-03-22 17:34:03 +0000

    finish

--- main.cs
+++ main.cs
@@ -1,7 +1,14 @@
+// Created by: Evgeny
+// Created on: Mar 2022
+// This program displays, "Hello, World!"
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("\nDone.");
   }
 }
