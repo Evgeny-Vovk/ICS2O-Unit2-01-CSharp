cf0038096756a235b0ea03fec3a42ba9411b3b08
Evgeny-Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-04-04 16:51:32 +0000

    fix

--- main.cs
+++ main.cs
@@ -8,7 +8,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-    Console.WriteLine("Hello, World!");
-    Console.WriteLine("\nDone.");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
     }
 }
